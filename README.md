88d8fc7ea6fa0a97204efccf2d1b3e9c46281b8c
Fernando <14892931+FMR7@users.noreply.github.com>
2022-06-25 00:08:59 +0200

    Init

---
+++ README.md
@@ -0,0 +1,5 @@
+# Factorio Faster Robots
+Improves base speed of all the inserters.
+
+# Mod Page
+https://mods.factorio.com/mod/faster-inserters
---
+++ settings.lua
@@ -0,0 +1,9 @@
+data:extend({{
+    type = "double-setting",
+    name = "faster-inserters-speed",
+    localised_name = "Inserter speed multiplier [img=info]",
+    localised_description = "To get the base game speed use 1 as multiplier",
+    setting_type = "startup",
+    minimum_value = 1,
+    default_value = 5
+}})
